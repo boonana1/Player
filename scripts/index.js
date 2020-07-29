1f47a013bf7a8be103c340ee25e6ae6250d4eb6d
boonana1 <dima999366@gmail.com>
2020-07-29 22:32:19 +0500

    Третий урок

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+import { radioPlayerInit } from './radioPlayer.js';
+import { musicPlayerInit } from './musicPlayer.js';
+import { videoPlayerInit } from './videoPlayer.js';
+
+const playerBtn = document.querySelectorAll('.player-btn');
+const playerBlock = document.querySelectorAll('.player-block');
+const temp = document.querySelector('.temp');
+
+const deactivationPlayer = () => {
+  temp.getElementsByClassName.display = 'none';
+  playerBlock.forEach((item) => {
+    item.classList.remove('active');
+  });
+  playerBtn.forEach((item) => {
+    item.classList.remove('active');
+  });
+};
+
+playerBtn.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    deactivationPlayer();
+    btn.classList.add('active');
+    playerBlock[index].classList.add('active');
+  });
+});
+
+videoPlayerInit();
+radioPlayerInit();
+musicPlayerInit();
